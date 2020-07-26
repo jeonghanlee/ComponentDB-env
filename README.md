2c911aa364321e3d64d393b2a750921874224ac5
Jeong Han Lee <jeonghan.lee@gmail.com>
2020-07-26 02:43:58 -0700

    add the same functionality of configure_web_portal

--- README.md
+++ README.md
@@ -12,44 +12,4 @@ make init
 
 ```bash
 make install.pkgs
-source docker/scripts/.sourceme
-```
-
-```bash
-$ tree -L 1
-.
-├── [jhlee    4.0K]  ComponentDB-src
-├── [jhlee    4.0K]  configure
-├── [jhlee    4.0K]  data
-├── [jhlee    4.0K]  docker
-├── [jhlee    4.0K]  docs
-├── [jhlee    4.0K]  etc
-├── [jhlee     18K]  LICENSE
-├── [jhlee     916]  Makefile
-├── [jhlee     586]  README.md
-├── [jhlee    4.0K]  site-template
-└── [jhlee    4.0K]  support-'short_hostname`
-```
-
-## Build support needed for the application
-
-```bash
-make support
-```
-
-## load enviornment variables with new support built
-
-```bash
-source ComponentDB-src/setup.sh
-```
-
-## Create deployment configuration
-
-```bash
-etc/cdb.deploy.conf
-etc/cdb.openssl.cnf
-```
-
-```bash
-make config
 ```
--- docker/scripts/required_pkgs.sh
+++ docker/scripts/required_pkgs.sh
@@ -5,10 +5,19 @@
 #  date    : Monday, June 29 14:02:20 PDT 2020
 #  version : 0.0.1
 
-declare -gr SC_SCRIPT="$(realpath "$0")"
-declare -gr SC_SCRIPTNAME=${0##*/}
-declare -gr SC_TOP="${SC_SCRIPT%/*}"
 
+declare -g SC_SCRIPT;
+#declare -g SC_SCRIPTNAME;
+declare -g SC_TOP;
+#declare -g LOGDATE;
+
+SC_SCRIPT="$(realpath "$0")";
+#SC_SCRIPTNAME=${0##*/};
+SC_TOP="${SC_SCRIPT%/*}"
+#LOGDATE="$(date +%y%m%d%H%M)"
+
+function pushd { builtin pushd "$@" > /dev/null || exit; }
+function popd  { builtin popd  > /dev/null || exit; }
 
 function find_dist
 {
@@ -19,10 +28,10 @@ function find_dist
      	dist_id=$(lsb_release -is)
      	dist_cn=$(lsb_release -cs)
      	dist_rs=$(lsb_release -rs)
-     	echo $dist_id ${dist_cn} ${dist_rs}
+     	echo "$dist_id" "${dist_cn}" "${dist_rs}"
     else
      	eval $(cat /etc/os-release | grep -E "^(PRETTY_NAME)=")
-	echo ${PRETTY_NAME}
+	echo "${PRETTY_NAME}"
     fi
 }
 
@@ -30,26 +39,65 @@ function find_dist
 function define_python_path
 {
     local pythonpath="$1"
-    echo "PYTHONPATH=${pythonpath}" > ${SC_TOP}/.sourceme
-    echo "export PYTHONPATH"       >> ${SC_TOP}/.sourceme
-    chmod +x ${SC_TOP}/.sourceme
+    echo "PYTHONPATH=${pythonpath}" > "${SC_TOP}/.sourceme"
+    echo "export PYTHONPATH"       >> "${SC_TOP}/.sourceme"
+    chmod +x "${SC_TOP}/.sourceme"
     
 }
 
 function debian_pkgs
 {
+    ## The following packages are listed in install_python_packages.sh via `make support`
+    ## ComponentDB-src/support/bin/install_python_packages.sh 
+    ## python-setuptools \
+    ## python-click \
+    ## python2.7-ldap \
+    ## python-pip \
+    ## python-sphinx \
+    ## twine \
+    ## python-cherrypy \
+    ## python-routes \
+    ## python-sqlalchemy \
+    ## python-mysqldb \
+    ## python-suds 
+    ## ---------------------
     ## Debian 10
     apt update
     apt install -y \
-    	wget curl expect \
-    	git openssl \
-    	sed gawk unzip \
-    	make cmake build-essential gcc \
-    	libssl-dev libldap2-dev libsasl2-dev libcurses-ocaml-dev  \
-    	mariadb-server mariadb-client  libmariadbclient-dev
-
+    	wget \
+        curl \
+        expect \
+    	git \
+        openssl \
+    	sed \
+        gawk \
+        unzip \
+        make \
+        cmake \
+        build-essential \
+        gcc \
+    	libssl-dev \
+        libldap2-dev \
+        libsasl2-dev \
+        libcurses-ocaml-dev \
+    	mariadb-server \
+        mariadb-client  \
+        libmariadbclient-dev \
+        maven \
+        ant \
+        python-setuptools \
+        python-click \
+        python2.7-ldap \
+        python-pip \
+        python-sphinx \
+        twine \
+        python-cherrypy \
+        python-routes \
+        python-sqlalchemy \
+        python-mysqldb \
+        python-suds 
     
-    ln -sf $(which mariadb_config) /usr/bin/mysql_config
+    ln -sf "$(which mariadb_config)" /usr/bin/mysql_config
     # MySQL-python-1.2.5 doesn't work with mariadb 
     # https://lists.launchpad.net/maria-developers/msg10744.html
     # https://github.com/DefectDojo/django-DefectDojo/issues/407
@@ -62,7 +110,6 @@ function debian_pkgs
 ## Do not test it yet Monday, June 29 15:09:24 PDT 2020
 function centos_pkgs
 {
-    ## Debian 10
     yum update
     yum install -y \
 	wget curl expect \
@@ -72,7 +119,7 @@ function centos_pkgs
 	zlib-devel openssl-devel openldap-devel readline-devel \
 	mariadb-server mariadb-libs
 	
-    ln -sf $(which mariadb_config) /usr/bin/mysql_config  
+    ln -sf "$(which mariadb_config)" /usr/bin/mysql_config  
 }
 
 
@@ -81,13 +128,13 @@ dist="$(find_dist)"
 case "$dist" in
     *Debian*) debian_pkgs ;;
     *Ubuntu*) debian_pkgs ;;
-    *CentOS*) centos_pkgs ;;
-    *RedHat*) centos_pkgs ;;
+#    *CentOS*) centos_pkgs ;;
+#    *RedHat*) centos_pkgs ;;
     *)
-	printf "\n";
-	printf "Doesn't support the detected $dist\n";
-	printf "Please contact jeonghan.lee@gmail.com\n";
-	printf "\n";
+	printf "\\n";
+	printf "Doesn't support the detected %s\\n" "$dist";
+	printf "Please contact jeonghan.lee@gmail.com\\n";
+	printf "\\n";
 	exit;
 	;;
 esac
